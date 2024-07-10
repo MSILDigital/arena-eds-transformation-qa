98a8279c0ad99324c64e3bceda6ca47b2e906f65
sumityadav <sumit.yadav@nagarro.com>
2024-07-10 21:23:08 +0530

    ADB-3716- ESlint fix.

--- blocks/arena-help-me-select/arena-help-me-select.js
+++ blocks/arena-help-me-select/arena-help-me-select.js
@@ -1,13 +1,13 @@
-import utility from "../../utility/utility.js";
+import utility from '../../utility/utility.js';
 
 export default function decorate(block) {
   const [...helpSelectEl] = block.children;
   const teasers = helpSelectEl.map((item) => {
     const currentElement = item?.firstElementChild;
     if (
-      currentElement &&
-      currentElement.childNodes.length > 0 &&
-      currentElement !== undefined
+      currentElement
+      && currentElement.childNodes.length > 0
+      && currentElement !== undefined
     ) {
       return currentElement;
     }
@@ -36,37 +36,37 @@ export default function decorate(block) {
     car3ImgAlt,
   ] = filteredTeasers;
 
-  const titleNode = title.querySelector("p");
-  titleNode.classList.add("title");
-  const subtitle = content.querySelector("p");
-  subtitle.classList.add("subtitle");
+  const titleNode = title.querySelector('p');
+  titleNode.classList.add('title');
+  const subtitle = content.querySelector('p');
+  subtitle.classList.add('subtitle');
 
   const newHtml = `
     <div class="helpme-select">
       <div class="content__container">
         <div class="left__content">
-          ${title ? title.innerHTML : ""}
-          ${content ? content.innerHTML : ""}
-          ${actionCtaContent ? actionCtaContent.innerHTML : ""}
+          ${title ? title.innerHTML : ''}
+          ${content ? content.innerHTML : ''}
+          ${actionCtaContent ? actionCtaContent.innerHTML : ''}
         </div>
         <div class="right__content">
           <div class="car__carousel">
             <div class="image-container">
-              ${car1LogoImg ? car1LogoImg.innerHTML : ""}
-              ${car1Img ? car1Img.innerHTML : ""}
+              ${car1LogoImg ? car1LogoImg.innerHTML : ''}
+              ${car1Img ? car1Img.innerHTML : ''}
             </div>
             <div class="image-container">
-              ${car2LogoImg ? car2LogoImg.innerHTML : ""}
-              ${car2Img ? car2Img.innerHTML : ""}
+              ${car2LogoImg ? car2LogoImg.innerHTML : ''}
+              ${car2Img ? car2Img.innerHTML : ''}
             </div>
             <div class="image-container">
-              ${car3LogoImg ? car3LogoImg.innerHTML : ""}
-              ${car3Img ? car3Img.innerHTML : ""}
+              ${car3LogoImg ? car3LogoImg.innerHTML : ''}
+              ${car3Img ? car3Img.innerHTML : ''}
             </div>
           </div>
           <div class="white-vertical__rectangle">
           </div>
-          <div class="dummy__text">${dummyText ? dummyText.innerHTML : ""}</div>
+          <div class="dummy__text">${dummyText ? dummyText.innerHTML : ''}</div>
         </div>
       </div>
       <div class="vertical__rectangle"></div>
@@ -76,18 +76,18 @@ export default function decorate(block) {
     </div>
   `;
 
-  block.innerHTML = "";
-  block.insertAdjacentHTML("beforeend", utility.sanitizeHtml(newHtml));
+  block.innerHTML = '';
+  block.insertAdjacentHTML('beforeend', utility.sanitizeHtml(newHtml));
 
-  const primaryCta = document.querySelector(".button-container a");
-  primaryCta.classList.add("primary__btn");
-  primaryCta.innerText = "";
+  const primaryCta = document.querySelector('.button-container a');
+  primaryCta.classList.add('primary__btn');
+  primaryCta.innerText = '';
 
-  const actionCTA = document.querySelector(".button-container a");
-  actionCTA?.setAttribute("target", targetEl.innerText);
-  actionCTA?.setAttribute("name", actionCtaLabel?.firstElementChild?.innerText);
+  const actionCTA = document.querySelector('.button-container a');
+  actionCTA?.setAttribute('target', targetEl.innerText);
+  actionCTA?.setAttribute('name', actionCtaLabel?.firstElementChild?.innerText);
 
-  const headings = document.querySelectorAll(".left__content h2");
+  const headings = document.querySelectorAll('.left__content h2');
   if (headings.length > 0) {
     headings.forEach((heading, index) => {
       heading.classList.add(`heading${index + 1}`);
@@ -95,7 +95,7 @@ export default function decorate(block) {
   }
 
   const carouselItems = document.querySelectorAll(
-    ".right__content .car__carousel .image-container"
+    '.right__content .car__carousel .image-container',
   );
 
   // Adding Logo alt text
@@ -103,8 +103,8 @@ export default function decorate(block) {
   if (logoAltText.length > 0) {
     logoAltText.forEach((logotext, index) => {
       const imageItem = carouselItems.item(index);
-      const logoTag = imageItem.querySelector("picture img");
-      logoTag?.setAttribute("alt", logotext?.firstElementChild?.innerText);
+      const logoTag = imageItem.querySelector('picture img');
+      logoTag?.setAttribute('alt', logotext?.firstElementChild?.innerText);
     });
   }
 
@@ -113,19 +113,18 @@ export default function decorate(block) {
   if (carAltText.length > 0) {
     carAltText.forEach((carText, index) => {
       const imageItem = carouselItems.item(index);
-      const carTag =
-        imageItem.querySelectorAll("picture img").length > 0 &&
-        imageItem.querySelectorAll("picture img")[1];
-      carTag?.setAttribute("alt", carText?.firstElementChild?.innerText);
+      const carTag = imageItem.querySelectorAll('picture img').length > 0
+        && imageItem.querySelectorAll('picture img')[1];
+      carTag?.setAttribute('alt', carText?.firstElementChild?.innerText);
     });
   }
 
   if (carouselItems.length > 0) {
     carouselItems.forEach((imageContainer) => {
-      const imageTag = imageContainer.querySelectorAll("picture");
+      const imageTag = imageContainer.querySelectorAll('picture');
       if (imageTag.length > 0) {
-        imageTag[0]?.classList.add("carLogo");
-        imageTag[1]?.classList.add("carImage");
+        imageTag[0]?.classList.add('carLogo');
+        imageTag[1]?.classList.add('carImage');
       }
     });
   }
@@ -135,9 +134,7 @@ export default function decorate(block) {
 
   function setActiveItem(index) {
     // Reset all items
-    carouselItems.forEach((item) =>
-      item.classList.remove("active", "next", "prev")
-    );
+    carouselItems.forEach((item) => item.classList.remove('active', 'next', 'prev'));
 
     // Calculate indices for active, next, and previous items
     const activeIndex = (index + totalItems) % totalItems;
@@ -145,18 +142,17 @@ export default function decorate(block) {
     const prevIndex = (activeIndex - 1 + totalItems) % totalItems;
 
     // Apply classes to the active, next, and previous items
-    carouselItems[activeIndex].classList.add("active");
-    carouselItems[nextIndex].classList.add("next");
-    carouselItems[prevIndex].classList.add("prev");
+    carouselItems[activeIndex].classList.add('active');
+    carouselItems[nextIndex].classList.add('next');
+    carouselItems[prevIndex].classList.add('prev');
 
     // Trigger reflow to apply CSS transitions
   }
 
   function moveCarousel(direction) {
-    currentIndex =
-      direction === "next"
-        ? (currentIndex + 1) % totalItems
-        : (currentIndex - 1 + totalItems) % totalItems;
+    currentIndex = direction === 'next'
+      ? (currentIndex + 1) % totalItems
+      : (currentIndex - 1 + totalItems) % totalItems;
     setActiveItem(currentIndex);
   }
 
@@ -164,6 +160,6 @@ export default function decorate(block) {
   setActiveItem(currentIndex);
 
   setInterval(() => {
-    moveCarousel("next");
+    moveCarousel('next');
   }, 10000);
 }
