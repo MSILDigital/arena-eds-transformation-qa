218a72e582a57dbcdc7cb239a48a3a41dc74ef68
varunmaisuria <varun.maisuria@nagarro.com>
2024-07-19 13:06:00 +0530

    image banner

---
+++ blocks/image-banner/image-banner.js
@@ -0,0 +1,10 @@
+import utility from '../../utility/utility.js';
+
+export default async function decorate(block) {
+    const [
+        desktopImageEl,
+        imgAltEl,
+        mobileImageEl
+    ] = block.children;
+
+}
