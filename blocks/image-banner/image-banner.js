09eb83d6a9853869ad6f8d66aa811a877f5ec17d
Priyansh <priyansh.ankre@nagarro.com>
2024-07-22 13:13:24 +0530

    eslint fix

--- blocks/image-banner/image-banner.js
+++ blocks/image-banner/image-banner.js
@@ -1,26 +1,26 @@
-import utility from "../../utility/utility.js";
+import utility from '../../utility/utility.js';
 
-const isMobile = () => window.matchMedia("(max-width: 768px)").matches;
+const isMobile = () => window.matchMedia('(max-width: 768px)').matches;
 
 function updateHtml(block, desktopImage, mobileImage) {
   const newHtml = `<div class="image-wrapper">${
     isMobile() ? mobileImage.outerHTML : desktopImage.outerHTML
   }</div>`;
-  block.innerHTML = "";
-  block.insertAdjacentHTML("beforeend", utility.sanitizeHtml(newHtml));
+  block.innerHTML = '';
+  block.insertAdjacentHTML('beforeend', utility.sanitizeHtml(newHtml));
 }
 
 export default async function decorate(block) {
   const [desktopImageEl, imgAltEl, mobileImageEl] = block.children;
-  const desktopImage = desktopImageEl.querySelector("picture");
-  const mobileImage = mobileImageEl.querySelector("picture");
+  const desktopImage = desktopImageEl.querySelector('picture');
+  const mobileImage = mobileImageEl.querySelector('picture');
 
   utility.initImage(desktopImage, imgAltEl);
   utility.initImage(mobileImage, imgAltEl);
 
   updateHtml(block, desktopImage, mobileImage);
 
-  window.addEventListener("resize", () => {
+  window.addEventListener('resize', () => {
     updateHtml(block, desktopImage, mobileImage);
   });
 }
